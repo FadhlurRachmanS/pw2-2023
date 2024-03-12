996da70b8ec90b1557ab8f25b2db8b9c87b16778
FadhlurRachmanS <146059527+FadhlurRachmanS@users.noreply.github.com>
2024-03-12 14:29:55 +0700

    Add files via upload

---
+++ pertemuan3/Tugas 3/home.php
@@ -0,0 +1,17 @@
+<!-- Task 1
+Pisah kode di bawah ke dalam file-file yang sudah dibuat sebelumnya. -->
+
+<?php
+    include_once 'header.php'
+?>
+				<h1>Welcome Home!</h1>
+				<p>
+				    <!--
+				        Tuliskan Nama - NIM - Rombel 
+				    -->
+
+                    Fadhlur Rachman - 0110123047 - SI02
+				</p>
+<?php
+    include_once 'footer.php'
+?>
